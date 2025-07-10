c56b6c5cee3019bf2c2ba6c4ed41b1eada53a362
sonukumaryadav3207 <sonukumaryadav3207@gmail.com>
2025-07-10 19:17:19 +0530

    -updated player- with api

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,30 +1,47 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import './TitleCards.css'
 import cards_data from '../../assets/cards/Cards_data'
+import { Link } from 'react-router-dom';
 const TitleCards = ({ title, category }) => {
 
+  const [apiData, setApiData] = useState([]);
   const cardsRef = useRef();
+
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkMmExYjk0YmJiNGEwNjJkYjhmNzhkNGNhOGNiZmQwMiIsIm5iZiI6MTc1MjEzMTkxMy43MzksInN1YiI6IjY4NmY2OTQ5MTcyYWYxNmJhMjlhNTJmNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.plRBfv94trUBB_2KPqCogjBfdrv88QYPQgtJ63C_4EQ'
+    }
+  };
+
+
+
   const handleWheel = (event) => {
     event.preventDefault();
     cardsRef.current.scrollLeft += event.deltaY
   }
-  
+
   useEffect(() => {
+    fetch(`https://api.themoviedb.org/3/movie/${category ? category : "now_playing"}?language=en-US&page=1`, options)
+      .then(res => res.json())
+      .then(res => setApiData(res.results))
+      .catch(err => console.error(err));
     cardsRef.current.addEventListener('wheel', handleWheel)
   }, [])
 
   return (
 
     <div className='title-cards'>
-      <h2>{title?title:"Popular on Netflix"}</h2>
+      <h2>{title ? title : "Popular on Netflix"}</h2>
       <div className="card-list" ref={cardsRef}>
 
 
-        {cards_data.map((card, index) => {
-          return <div className="card" key={index}>
-            <img src={card.image} alt="" />
-            <p>{card.name}</p>
-          </div>
+        {apiData.map((card, index) => {
+          return<Link to={`/player/${card.id}`} className="card" key={index}>
+            <img src={`https://image.tmdb.org/t/p/w500` + card.backdrop_path} alt="" />
+            <p>{card.original_title}</p>
+          </Link>
         })}
 
 
